2a6525efd9388e1d5cb22043274e4e5f09ae1a51
Erik Peldan <epe@trifork.com>
2016-05-14 10:50:47 +0200

    Write an endpoint for listing the available web components

--- server.lisp
+++ server.lisp
@@ -6,11 +6,30 @@
 
 ;; Load dependencies
 (quicklisp:quickload "hunchentoot")
+(quicklisp:quickload "yason")
 
-;; TODO create a url handler macro that can extract variable names from the url!
+(defvar server (make-instance 'easy-acceptor :port 4242))
+
+
+(defvar components nil)
+; (defvar signals nil)
+
+
 (define-easy-handler (list-stores :uri "/stores") ()
   (setf (content-type*) "text/plain")
   (format nil "Testing 1 2 3"))
 
 
-(defvar server (make-instance 'easy-acceptor :port 4242))
+
+(define-easy-handler (list-components :uri "/components") ()
+  (setf (content-type*) "text/plain")
+  
+  (yason:with-output-to-string* ()
+    
+    ;; TODO write encode-array JSON fn..
+    (yason:with-array ()
+      (loop for c in components
+	   do (yason:encode-array-element c)))))
+
+
+
