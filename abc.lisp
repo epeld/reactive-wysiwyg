6df4121f94d09f77bd939309ced9b9e79750945c
Erik Peldan <epe@trifork.com>
2016-05-17 21:45:13 +0200

    Clean up

--- abc.lisp
+++ abc.lisp
@@ -5,7 +5,7 @@
 (in-package :psx)
 
 (defun attrs (sexp)
-  "Extract the attributes part from ab HTML-sexp"
+  "Extract the attributes part from an HTML-sexp"
   (let (attr-list body)
     (loop for rest on sexp by #'cddr
        if (keywordp (first rest))
@@ -19,6 +19,7 @@
 
 
 (defun flatten-alist (alist)
+  "Returns keys and values interspersed in a flat list"
   (let (list)
     (loop for item in alist
 	 do (setf list (cons (car item) (cons (cdr item) list))))
@@ -26,55 +27,48 @@
 
 
 
-
 (defmacro psx (body)
-  "Anaphoric macro that captures psx-element and psx-attrs from its environment"
-  (if (atom body)
-      body
-      (let ((first (first body)))
-	(etypecase first
-      
-	  ;; keyword means builtin DOM element
-	  (keyword
-	   (multiple-value-bind (attrs children) (attrs (rest body))
+  "Compile html-like DSL using captured psx-element, psx-attrs, psx-children"
+  (if (and (consp body) (typep (first body) 'keyword))
+      (multiple-value-bind (attrs children) (attrs (rest body))
 	 
-	     ;; create-element will look for an installed component,
-	     ;; or use a builtin one (if keyword!)
-	     `(psx-element ,first (psx-attrs ,@(flatten-alist attrs)) 
+	 `(psx-element ,(first body)
+ 
+		       (psx-attrs ,@(flatten-alist attrs)) 
 	     
-			   ;; Compile all the children
-			   ,@(loop for child in children collect `(psx ,child)))))
-	
-	  ;; Symbol means 'call this function'
-	  (symbol
-	   ;; Try just passing the form through
-	   body)))))
-
-
+		       ;; Compile all the children
+		       (psx-children ,@(loop for child in children collect `(psx ,child)))))
+      
+      ;; Pass everything else through
+      body))
 
 
 (defmacro psx-to-who (psx)
-  `(macrolet ((psx-attrs (&body body)
-		`(quote ,body))
+  "Compile psx to cl-who syntax"
+  `(macrolet ((psx-attrs (&body kvs)
+		`(list ,@kvs))
 	      
-	      (psx-element (name attrs &rest children)
-		`(append (list ,name) ,attrs (list ,@children))))
+	      (psx-children (&body children)
+		`(list ,@children))
+	      
+	      (psx-element (name attrs children)
+		`(append (list ,name) ,attrs ,children)))
     
      (psx ,psx)))
 
 
-(defmacro who-to-html (who)
-  `(cl-who:with-html-output-to-string (s)
-     ,who))
-
 
 (defmacro psx-to-html (psx)
-  `(who-to-html ,(eval `(psx-to-who ,psx))))
+  "Shortcut for converting psx directly to an HTML string"
+  `(cl-who:with-html-output-to-string (s) 
+     ,(eval `(psx-to-who ,psx))))
 
-(psx-to-who (:div :class "abc" "Hello, World!"))
+
+(psx-to-who (:div :class "abc" "Hello, World!" "."))
+
+
 (psx-to-html (:div :class "abc" (:p "Hello, World!")
 		   (:p "blub")
 		   (:div (:span "erik"))))
-(psx-to-html (mytest2 2 "AA"))
 
 
