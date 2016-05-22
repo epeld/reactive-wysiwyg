be07e8dbbf936ed7e14d322b6fa2a4cf3debe793
Erik Peldan <epe@trifork.com>
2016-05-22 10:51:25 +0200

    Work on component dispatch

--- component2.lisp
+++ component2.lisp
@@ -1,8 +1,16 @@
 
 (defpackage :component
   (:use :common-lisp :hunchentoot)
-  (:import-from :peldan.string :string-bind-case)
-  (:import-from :peldan.resource :resource :find-resource :resource-name))
+  
+  (:import-from :peldan.string 
+		:string-bind-case)
+  
+  (:import-from :peldan.resource 
+		
+		:replace-resource
+		:resource
+		:find-resource
+		:resource-name))
 
 
 (in-package :component)
@@ -29,21 +37,29 @@
 					       :args (quote ,args)
 					       :name ,sname))
 	    ,(when publish
-		   `(pushnew ,name *components*)))))
+		   `(replace-resource ,name *components*)))))
 
 
-(defun generate-component-output (name output)
+(defun component-page (component output)
+  (format nil "You have requested the componet ~s as ~s" name output))
+
+
+(defun process-component-request (name output)
   (let ((component (find-resource name *components*)))
     (if component
-	(format nil "You have requested the componet ~s as ~s" name output)
+	(component-page component output)
 	(format nil "Component ~a doesn't exist" name))))
 
 
 ;; The sole handler for all requests
-(defun handle-component-request ()
+(defun dispatch-on-script-name ()
   (string-bind-case (script-name*)
+		    
 		    (("component" name output)
-		     (generate-component-output name output))
+		     (process-component-request name output))
+		    
+		    (("data" name)
+		     (format nil "The data-page is still work in progress (~s)" name))
 		    
 		    (otherwise
 		     (format nil "Page not found! ~s" (script-name*)))))
@@ -53,21 +69,17 @@
 
 ;; Set up the server
 (define-easy-handler (components-handler :uri (constantly t)) ()
-  (handle-component-request))
+  (dispatch-on-script-name))
 
-(defparameter server (make-instance 'easy-acceptor :port 4243))
+(defvar server (make-instance 'easy-acceptor :port 4243))
 
-(start server)
+(quote (start server))
 
 
 
 
 (setf *show-lisp-errors-p* t)
 
-(defcomponent testdiv (a b)
-  (:div "This is a test div" a (:p "This is a paragraph!") (mapcar (lambda (x) `(:p "Count" ,(write-to-string x))) (list 0 1 2 3)) b))
+(quote (defcomponent testdiv (a b)
+  (:div "This is a test div" a (:p "This is a paragraph!") (mapcar (lambda (x) `(:p "Count" ,(write-to-string x))) (list 0 1 2 3)) b)))
 
-(setf *components* nil)
-(pushnew testdiv *components*)
-
-(resource-name testdiv)
