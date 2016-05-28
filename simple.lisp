cf3c1d3fe8b11133c24ff2117ab28e30b247627d
Erik Peldan <epe@trifork.com>
2016-05-28 21:42:47 +0200

    Work on an example page..

--- simple.lisp
+++ simple.lisp
@@ -1,15 +1,39 @@
 
-(defpackage :peldan.simple
-  (:use :common-lisp :parenscript))
+(defpackage peldan.simple
+  (:use common-lisp parenscript)
+  (:import-from parenscript ps ps*)
+  (:import-from cl-who str)
+  (:import-from peldan.component 
+		defcomponent
+		defcomponent-macro
+		render
+		component))
+
+(in-package peldan.simple)
+
+
+(defcomponent-macro javascript (&body ps)
+  `(:script :type "text/javascript" (str (ps ,@ps))))
+
+
+(defcomponent notice ()
+  (:p "Please wait a while as the connection is being set up.."))
+
+
+(defcomponent hello-world ()
+  (:div (:h1 "Hello, World")
+	(component #'notice)
+	(:iframe)
+	(javascript ((ps:@ console log) "Hi"))))
 
 
 (defun simple-handler (request)
   (declare (ignore request))
-  (render #'hello-world)
-  "Hello, World!")
+  (render hello-world))
 
 
-(psx ())
+(render hello-world)
+;(psx ())
 
 
-(pushnew #'simple-handler peldan.dispatch:*handlers*)
+;(pushnew (lambda (req) (simple-handler req)) peldan.dispatch:*handlers*)
