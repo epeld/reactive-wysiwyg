b97b82743126cca0d8a044e8e22873d86a2494dd
Erik Peldan <epe@trifork.com>
2016-05-29 11:41:02 +0200

    Delete obsolete files

--- js-signal.lisp
+++
@@ -1,8 +0,0 @@
-;; TODO from some kind of blueprint (JSON?) create some kind of graph with a WS-signal on top!
-
-
-;; TOP Signal JSON
-;; 2nd Store Signals; filter JSON keys and compare values
-;; Then: standard signal graph
-
-;; { intSignal: { intSignalSubSignal: { fn: "add" } } }
