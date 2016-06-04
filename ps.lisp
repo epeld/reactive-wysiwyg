542f24ba7d86e1968182288838ab1dc862c9dd3d
Erik Peldan <epe@trifork.com>
2016-06-04 23:07:31 +0200

    Two utils for including/importing PS and JS code

--- ps.lisp
+++ ps.lisp
@@ -14,3 +14,15 @@
 (defmacro defsnippet (name lambda-list &body body)
   `(defun ,name ,lambda-list
      (ps ,@body)))
+
+
+;; To allow including other files a la npm et al
+(defpsmacro load (filename)
+  (ps-compile-file filename))
+
+
+(defpsmacro include (filename)
+  (with-open-file (s (concatenate 'string "js/" filename))
+    (loop for line = (read-line s nil 'foo)
+       until (eq line 'foo)
+       do (write-line line ps:*parenscript-stream*))))
