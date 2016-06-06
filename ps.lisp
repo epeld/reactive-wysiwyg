4a9178146774b42e8891d3c6f9de9bdec257e5f7
Erik Peldan <epe@trifork.com>
2016-06-06 12:08:19 +0200

    Write utils for working with JSON in JS

--- ps.lisp
+++ ps.lisp
@@ -10,6 +10,12 @@
   `((@ console warn) ,@args))
 
 
+(defpsmacro json-stringify (object)
+  `((@ j-s-o-n stringify) ,object))
+
+
+(defpsmacro json-parse (string)
+  `((@ j-s-o-n parse) ,string))
 
 ;; To allow including other files a la npm et al
 (defpsmacro load (filename)
