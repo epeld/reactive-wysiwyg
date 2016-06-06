849714bf2ac9436dac7e5b6291f38025eef12b7c
Erik Peldan <epe@trifork.com>
2016-06-06 12:00:58 +0200

    Simplify JS setup

--- simple.lisp
+++ simple.lisp
@@ -13,30 +13,46 @@
 
 
 (defun test-component ()
-  '(:div "This is a virtual dom element" 
+  '(if (@ state debug)
+    (psx (:div ((@ -j-s-o-n stringify) state nil "    ")))
+    (psx (:div "This is a virtual dom element" 
      
-     (mapcar (lambda (x)
-	       (psx (:p "Value:" x)))
-      state)
+	  (mapcar (lambda (x)
+		    (psx (:p "Value:" x)))
+		  state)
      
-     (:div 
-      "And this is the end of it. (Rendered " 
-      (length state)
-      " elements)")
+	  (:div 
+	   "And this is the end of it. (Rendered " 
+	   (length state)
+	   " elements)")
      
-     (:textarea
-      :value ((@ -j-s-o-n stringify) state))))
+	  (:textarea
+	   :value ((@ -j-s-o-n stringify) state))))))
 
+
+(defun application-js (&optional initial-state)
+  `(defvar App
+     (let ((module (create)))
+       
+       (setf (@ module state)
+	     ((@ -j-s-o-n parse) ,(json-string initial-state)))
+	      
+       (setf (@ module update)
+	     ,(render-ps (test-component)
+			 `(@ module state)))
+     
+       (setf (@ module ws)
+	     ,(connect-ps `(@ module update)))
+     
+       module)))
 
 
 (defcomponent hello-world ()
   (:div (:h1 "This is an example of using Virtual DOM")
 	(javascript
 	  (read-virtual-dom-js)
-	  (ps* 
-	   *ps-lisp-library*
-	   (render-loop (test-component)
-			(list 1 2 "haj"))))))
+	  (ps* *ps-lisp-library* 
+	       (application-js (list 2 3 "bluu"))))))
 
 
 (defun simple-handler (request)
--- virtual-dom.lisp
+++ virtual-dom.lisp
@@ -6,35 +6,24 @@
   (read-file-to-string "js/virtual-dom.js"))
 
 
-
-(ps:defpsmacro json (data)
-  `(lisp (encode ,data)))
-
-
 (defun json-string (data)
   (with-output-to-string (s)
     (encode data s)))
 
 
-(defun make-renderer (html state)
+(defun render-ps (html state)
   `(flet ((render (state)
 		 (psx ,html)))
     
-     (let* ((state ((@ -j-s-o-n parse) ,(json-string state)))
-	    (tree (render state))
+     (let* ((tree (render ,state))
 	    (element (reify tree)))
 	    
        ((ps:@ document body append-child) element)
 	
        (lambda (new-state)
-	 (setf state new-state)
+	 (setf ,state new-state)
 	 (let* ((new-tree (render new-state))
 		(patches (diff-tree tree new-tree)))
 	   (setf tree new-tree)
 	   (apply-patch element patches))))))
 
-
-(defun render-loop (html state)
-  `(defvar
-       set-state
-     ,(make-renderer html state)))
