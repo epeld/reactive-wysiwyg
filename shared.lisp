39f8e384ec87aa094d71bda68ef933341ddc071f
Erik Peldan <epe@trifork.com>
2016-06-07 21:54:41 +0200

    Fix issues

--- shared.lisp
+++ shared.lisp
@@ -3,19 +3,18 @@
 
 (defgroup code)
 
-;; TODO figure out how to 'tag' PS code to be able to filter them etc
+
 (defmacro defun+ps (name lambda-list &body body)
-  (add-code (make-code name
-		       :args lambda-list
-		       :body body))
+  (replace-code (make-code name
+			   :args lambda-list
+			   :body body))
   `(defun ,name ,lambda-list
 	 ,@body))
 
 
-;; TODO (name item) will be a string but we want the symbol. fix!
-(defun generate-ps (&optional (type 'defun))
+(defun generate-ps ()
   "Generate PS code from the stored function definitions"
   `(progn 
      ,@(loop for item in (members code-group) collect
-	    `(,type ,(name item) ,(field-value :args item)
+	    `(defun ,(field-value :symbol item) ,(field-value :args item)
 		    ,@(field-value :body item)))))
