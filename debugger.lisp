093ee3e961542a8ef0dc542e1f4f636ab1989c0a
Erik Peldan <epe@trifork.com>
2016-06-13 21:10:49 +0200

    Convert action-list into a function instead of a component

--- debugger.lisp
+++ debugger.lisp
@@ -1,29 +1,28 @@
 
 (in-package :peldan.debugger)
 
-(peldan.component:register-component
- 'action-list ()
- ;; TODO this is actually evaluated during "component compile-time".
- ;; figure out a way to make it happen when component is 'called'!
- (let ((action-names (mapcar (lambda (action)
-			       (string-downcase (peldan.resource:name action)))
-			     (peldan.action:list-of-actions))))
-   `(:div "Available Actions:"
-	  (:select (mapcar (lambda (action)
-			     (psx (:option action)))
-			   (list ,@action-names))))))
+
+(defun action-list (actions)
+  (let ((action-names (mapcar (lambda (action)
+				(string-downcase (peldan.resource:name action)))
+			      actions)))
+    `(psx (:div "Available Actions:"
+		(:select (mapcar (lambda (action)
+				   (psx (:option action)))
+				 (list ,@action-names)))))))
 
 
 (peldan.component:register-component 
  'debugger ()
  `(:div (:h2 "Debbuging!")
 	(:pre ((@ -j-s-o-n stringify) (@ peldan.component:state component state) nil "    "))
-	(peldan.component:subcomponent action-list nil)
+	(lisp (action-list (peldan.action:list-of-actions)))
 	(:button :onclick (action debug)
 		 "Back")))
 
 
-;; TODO create a dropdown component for selecting actions
 
 
 ;; TODO create an action timeline component
+(sxhash 'peldan.ps:stringify)
+(sxhash 'stringify)
