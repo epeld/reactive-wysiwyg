01d13cbbb4569110c3dc3ac7bb41c2c31bab765a
Erik Peldan <epe@trifork.com>
2016-06-25 20:00:43 +0200

    Start working on a component for viewing session state

--- debugger.lisp
+++ debugger.lisp
@@ -10,3 +10,33 @@
 				  (peldan.virtual-dom:action "debug"))
 		       "Back"))))
 
+
+
+(defun request-handler (req)
+  (when (string= "/sessions" (hunchentoot:script-name req))
+    (peldan.component:generate-component-html
+     `(:div (:class "sessions")
+	    (:h1 "Open Sessions")
+	    ,@(loop for session in peldan.websocket:*sessions*
+		   collect 
+		   (let ((uuid (peldan.websocket:session-uuid session)))
+		     `(peldan.ml:h 
+		       (:div 
+			(:a (:href ,(format nil "/sessions/~a" uuid))
+			    ,(concatenate 'string
+					  "#"
+					  uuid))
+			" - "
+			(:small
+			,(with-output-to-string (s)
+						(prin1 (peldan.websocket:session-state session)
+						       s))))))))
+     :uuid nil :state nil)))
+
+
+(ps:ps* `(peldan.ml:h (:div (:a (:href ,(concatenate 'string
+						     "#"
+						     "33"))
+					     "How does this work?"))))
+
+(pushnew 'request-handler peldan.dispatch:*handlers*)
